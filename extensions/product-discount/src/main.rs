cca4214ff1208136b2f319fe68ee910661632ebe
ablesense-hugh <hugh@ablesense.com>
2023-03-20 16:15:06 -0400

    deploy 10% discount for 2 line items

--- extensions/product-discount/src/main.rs
+++ extensions/product-discount/src/main.rs
@@ -1,38 +1,12 @@
 use shopify_function::prelude::*;
 use shopify_function::Result;
-
-use serde::{Deserialize, Serialize};
-
+use serde::{Serialize};
+// Use the shopify_function crate to generate structs for the function input and output
 generate_types!(
     query_path = "./input.graphql",
     schema_path = "./schema.graphql"
 );
-
-#[derive(Serialize, Deserialize, PartialEq)]
-#[serde(rename_all(deserialize = "camelCase"))]
-struct Configuration {
-    pub quantity: i64,
-    pub percentage: f64,
-}
-
-impl Configuration {
-    const DEFAULT_QUANTITY: i64 = 999;
-    const DEFAULT_PERCENTAGE: f64 = 0.0;
-
-    fn from_str(value: &str) -> Self {
-        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
-    }
-}
-
-impl Default for Configuration {
-    fn default() -> Self {
-        Configuration {
-            quantity: Self::DEFAULT_QUANTITY,
-            percentage: Self::DEFAULT_PERCENTAGE,
-        }
-    }
-}
-
+// Use the shopify_function crate to declare your function entrypoint
 #[shopify_function]
 fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
     let no_discount = output::FunctionResult {
@@ -40,46 +14,47 @@ fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
         discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
     };
 
-    let config = match input.discount_node.metafield {
-        Some(input::InputDiscountNodeMetafield { value }) =>
-            Configuration::from_str(&value),
-        None => return Ok(no_discount),
-    };
-
+    // Iterate all the lines in the cart to create discount targets
     let targets = input.cart.lines
         .iter()
-        .filter(|line| line.quantity >= config.quantity)
+        // Only include cart lines with a quantity higher than two
+        .filter(|line| line.quantity >= 2)
+        // Only include cart lines with a targetable product variant
         .filter_map(|line| match &line.merchandise {
             input::InputCartLinesMerchandise::ProductVariant(variant) => Some(variant),
             input::InputCartLinesMerchandise::CustomProduct => None,
         })
+        // Use the variant ID to create a discount target
         .map(|variant| output::Target {
             product_variant: Some(output::ProductVariantTarget {
                 id: variant.id.to_string(),
                 quantity: None,
-           })
+            })
         })
         .collect::<Vec<output::Target>>();
 
     if targets.is_empty() {
+        // You can use STDERR for debug logs in your function
         eprintln!("No cart lines qualify for volume discount.");
         return Ok(no_discount);
     }
 
+    // The shopify_function crate serializes your function result and writes it to STDOUT
     Ok(output::FunctionResult {
         discounts: vec![output::Discount {
             message: None,
+            // Apply the discount to the collected targets
             targets,
+            // Define a percentage-based discount
             value: output::Value {
                 fixed_amount: None,
                 percentage: Some(output::Percentage {
-                    value: config.percentage.to_string()
+                    value: "10.0".to_string()
                 })
             }
         }],
         discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
     })
 }
-
 #[cfg(test)]
 mod tests;
