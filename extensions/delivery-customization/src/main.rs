c11025799e864f339cf671c7c0fea94b0b47ca4b
ablesense-hugh <hugh@ablesense.com>
2023-03-27 10:21:32 -0400

    add delivery extension into the app

--- extensions/delivery-customization/src/main.rs
+++ extensions/delivery-customization/src/main.rs
@@ -1,37 +1,48 @@
 use shopify_function::prelude::*;
 use shopify_function::Result;
-
-use serde::{Deserialize, Serialize};
-
+use serde::{Serialize};
+// Use the shopify_function crate to generate structs for the function input and output
 generate_types!(
     query_path = "./input.graphql",
     schema_path = "./schema.graphql"
 );
-
-#[derive(Serialize, Deserialize, Default, PartialEq)]
-#[serde(rename_all(deserialize = "camelCase"))]
-struct Configuration {
-
-}
-
-impl Configuration {
-    fn from_str(value: &str) -> Self {
-        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
-    }
-}
-
+// Use the shopify_function crate to declare your function entrypoint
 #[shopify_function]
 fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
-    let no_changes = output::FunctionResult { operations: vec![] };
+    // The message we wish to add to the delivery option
+    let message = "May be delayed due to weather conditions";
 
-    let _config = match input.delivery_customization.metafield {
-        Some(input::InputDeliveryCustomizationMetafield { value }) =>
-            Configuration::from_str(&value),
-        None => return Ok(no_changes),
-    };
-    
-    Ok(output::FunctionResult { operations: vec![] })
+    let to_rename = input.cart.delivery_groups
+        .iter()
+        // Filter for delivery groups with a shipping address containing the affected state or province
+        .filter(|group| {
+            let state_province = group.delivery_address.as_ref()
+                .and_then(|address| address.province_code.as_ref());
+            match state_province {
+                Some(code) => code == "ON",
+                None => false
+            }
+        })
+        // Collect the delivery options from these groups
+        .flat_map(|group| &group.delivery_options)
+        // Construct a rename operation for each, adding the message to the option title
+        .map(|option| output::RenameOperation {
+            delivery_option_handle: option.handle.to_string(),
+            title: match &option.title {
+                Some(title) => format!("{} - {}", title, message),
+                None => message.to_string()
+            }
+        })
+        // Wrap with an Operation
+        .map(|rename| output::Operation {
+            rename: Some(rename),
+            hide: None,
+            move_: None
+        })
+        .collect();
+
+    // The shopify_function crate serializes your function result and writes it to STDOUT
+    Ok(output::FunctionResult { operations: to_rename })
 }
-
 #[cfg(test)]
 mod tests;
